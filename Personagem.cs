2c3f0b54328ca48c0950b2b8144e062af81aae3a
Matheus <matheus.barboza.henrique@gmail.com>
2020-05-22 15:42:11 -0300

    first

---
+++ Personagem.cs
@@ -0,0 +1,24 @@
+namespace PrimeiroPOO
+{
+    public class Personagem
+    {
+        public string nome;
+
+        public int idade;
+
+        public string armadura;
+
+        public float Atacar(float forca, float potencia )
+        {
+            return forca * potencia;
+        }
+
+
+        public string Defender()
+        {
+            return "O Personagem atacou !";
+        }
+
+
+    }
+}
